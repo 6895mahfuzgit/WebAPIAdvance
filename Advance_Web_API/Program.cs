9bfe5c64edfc39ea2098ba21410cdd77cf4cb02d
mahfuzroyex123 <mahfuzroyex123@gmail.com>
2020-06-14 02:02:58 +0600

    Added Swagger

--- Advance_Web_API/Program.cs
+++ Advance_Web_API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Advance_API
+namespace Advance_Web_API
 {
     public class Program
     {
